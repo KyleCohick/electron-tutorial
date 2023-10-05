ae0dda898b8da25146712a2030251b048ea6cdfe
Kyle Cohick <kycohick@amazon.com>
2023-10-05 14:36:00 -0400

    Added dark mode feature set and more IPC communication

--- renderer.js
+++ renderer.js
@@ -1,11 +1,22 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
-const func = async () => {
+document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+  const isDarkMode = await window.darkMode.toggle()
+  document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+})
+
+document.getElementById('reset-to-system').addEventListener('click', async () => {
+  await window.darkMode.system()
+  document.getElementById('theme-source').innerHTML = 'System'
+})
+
+// This function runs IPC between the renderer and the main process.
+const pingPong = async () => {
   const response = await window.versions.ping()
   const responseElement = document.getElementById('response')
   console.log(response) // prints out 'pong'
   responseElement.innerText = response
 }
 
-func()
+pingPong()
